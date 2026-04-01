7215ab3325a139d3064253029558a0693cacd2a7
Samiul <your@email.com>
2026-04-01 21:11:18 +0600

    feat(routes): register comment routes in central IndexRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import adminRoutes from "./admin.routes";
 import categoryRoutes from "./category.routes";
+import commentRoutes from "./comment.routes";
 import ideaRoutes from "./idea.routes";
 import paymentRoutes from "./payment.routes";
 import voteRoutes from "./vote.routes";
@@ -10,6 +11,7 @@ const router = Router();
 router.use("/categories", categoryRoutes);
 router.use("/ideas", ideaRoutes);
 router.use("/votes", voteRoutes);
+router.use("/comments", commentRoutes);
 router.use("/payments", paymentRoutes);
 router.use("/admin", adminRoutes);
 
